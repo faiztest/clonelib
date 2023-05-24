d032d09cf2f2ddd1d01dd92ed9237e5327e627a0
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-24 13:24:58 +0700

    Update 3 bidi.py

--- pages/3 bidi.py
+++ pages/3 bidi.py
@@ -134,7 +134,7 @@ if uploaded_file is not None:
         @st.cache_resource(ttl=3600)
         def arm_table():
             res = association_rules(freq_item, metric='confidence', min_threshold=conf) 
-            res = res[['antecedents', 'consequents', 'antecedents support', 'consequents support', 'support', 'confidence', 'lift', 'conviction']]
+            #res = res[['antecedents', 'consequents', 'antecedents support', 'consequents support', 'support', 'confidence', 'lift', 'conviction']]
             res['antecedents'] = res['antecedents'].apply(lambda x: ', '.join(list(x))).astype('unicode')
             res['consequents'] = res['consequents'].apply(lambda x: ', '.join(list(x))).astype('unicode')
 
