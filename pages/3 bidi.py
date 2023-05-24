d8337c0b4a2db121654dbc68beae3dbc5d2d0034
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-24 13:27:05 +0700

    Update 3 bidi.py

--- pages/3 bidi.py
+++ pages/3 bidi.py
@@ -128,11 +128,13 @@ if uploaded_file is not None:
         #===Association rules===
         @st.cache_resource(ttl=3600)
         def freqitem():
+            global supp, maxlen
             freq_item = fpgrowth(df, min_support=supp, use_colnames=True, max_len=maxlen)
             return freq_item
         
         @st.cache_resource(ttl=3600)
         def arm_table():
+            global conf
             res = association_rules(freq_item, metric='confidence', min_threshold=conf) 
             #res = res[['antecedents', 'consequents', 'antecedents support', 'consequents support', 'support', 'confidence', 'lift', 'conviction']]
             res['antecedents'] = res['antecedents'].apply(lambda x: ', '.join(list(x))).astype('unicode')
