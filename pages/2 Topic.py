2b6d91460d1a80010393c57f43a725ba7ff16710
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-24 09:07:09 +0700

    Update 2 Topic.py

--- pages/2 Topic.py
+++ pages/2 Topic.py
@@ -57,7 +57,7 @@ def clean_csv(scopus_file):
     paper = paper[~paper.Abstract.str.contains("STRAIT")]
             
         #===mapping===
-    paper['Abstract_pre'] = paper['Abstract'].map(lambda x: re.sub('[,:;\.!?•-]', '', x))
+    paper['Abstract_pre'] = paper['Abstract'].map(lambda x: re.sub('[,:;\.!?•-><=]', '', x))
     paper['Abstract_pre'] = paper['Abstract_pre'].map(lambda x: x.lower())
     paper['Abstract_pre'] = paper['Abstract_pre'].map(lambda x: re.sub('©.*', '', x))
             
