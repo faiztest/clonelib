784a6d2b768c8affed40714265b75937ff44518d
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-24 08:49:15 +0700

    Update 2 Topic.py

--- pages/2 Topic.py
+++ pages/2 Topic.py
@@ -74,7 +74,7 @@ def clean_csv(scopus_file):
     stop = stopwords.words('english')
     paper['Abstract_stop'] = paper['Abstract_lem'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
     topic_abs = paper.Abstract_stop.values.tolist()
-    return topic_abs, paper
+    return topic_abs
 
 #===upload file===
 uploaded_file = st.file_uploader("Choose a file")
@@ -86,7 +86,7 @@ if uploaded_file is not None:
         
     #===topic===
     if method == 'Choose...':
-        st.write(topic_abs)
+        st.write(uploaded_file)
 
     elif method is 'pyLDA':
          topic_abs_LDA = [t.split(' ') for t in topic_abs]
