9f4666811d093c20a067700ac734b062446bd843
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-12 13:35:42 +0700

    Update 2 Topic.py

--- pages/2 Topic.py
+++ pages/2 Topic.py
@@ -29,8 +29,8 @@ import bitermplus as btm
 import tmplot as tmp
 import tomotopy
 import sys
-import os
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+#import os
+#os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 #===config===
