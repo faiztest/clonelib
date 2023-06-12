763530b642a84f1aae655910f0d10c949b3f09f8
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-12 13:54:39 +0700

    Update 2 Topic.py

--- pages/2 Topic.py
+++ pages/2 Topic.py
@@ -29,9 +29,6 @@ import bitermplus as btm
 import tmplot as tmp
 import tomotopy
 import sys
-#import os
-#os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
 
 #===config===
 st.set_page_config(
@@ -40,12 +37,14 @@ st.set_page_config(
      layout="wide"
 )
 st.header("Topic Modeling")
-st.subheader('Put your CSV file here ...')
+st.subheader('Put your file here...')
 
+def reset_resource():
+     st.cache_resource.clear()
 
 def reset_all():
      st.cache_data.clear()
-
+     st.cache_resource.clear()
      
 #===clean csv===
 @st.cache_data(ttl=3600)
@@ -90,11 +89,8 @@ def conv_txt(file):
     col_dict = {'TI': 'Title',
             'SO': 'Source title',
             'DT': 'Document Type',
-            'DE': 'Author Keywords',
-            'ID': 'Keywords Plus',
             'AB': 'Abstract',
-            'TC': 'Cited by',
-            'PY': 'Year',}
+            'PY': 'Year'}
     papers = pd.read_csv(file, sep='\t', lineterminator='\r')
     papers.rename(columns=col_dict, inplace=True)
     return papers
@@ -124,8 +120,8 @@ if uploaded_file is not None:
         st.write('')
 
     elif method == 'pyLDA':
-         num_topic = st.slider('Choose number of topics', min_value=2, max_value=15, step=1, on_change=reset_all)
-         @st.cache_data(ttl=3600)
+         num_topic = st.slider('Choose number of topics', min_value=2, max_value=15, step=1, on_change=reset_resource)
+         @st.cache_resource(ttl=3600)
          def pylda():
             topic_abs_LDA = [t.split(' ') for t in topic_abs]
             id2word = Dictionary(topic_abs_LDA)
@@ -155,7 +151,7 @@ if uploaded_file is not None:
          #===visualization===
              with st.spinner('Calculating and Creating pyLDAvis Visualization ...'):
               py_lda_vis_html, coherence_lda = pylda()
-              st.write('Score: ', (coherence_lda))
+              st.write('Coherence: ', (coherence_lda))
               components.html(py_lda_vis_html, width=1700, height=800)
               st.markdown('Copyright (c) 2015, Ben Mabey. https://github.com/bmabey/pyLDAvis')
 
@@ -170,9 +166,9 @@ if uploaded_file is not None:
      
      #===Biterm===
     elif method == 'Biterm':
-        num_bitopic = st.slider('Choose number of topics', min_value=2, max_value=20, step=1, on_change=reset_all)     
+        num_bitopic = st.slider('Choose number of topics', min_value=2, max_value=20, step=1, on_change=reset_resource)     
         #===optimize Biterm===
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def biterm_topic():
             X, vocabulary, vocab_dict = btm.get_words_freqs(topic_abs)
             tf = np.array(X.sum(axis=0)).ravel()
@@ -196,12 +192,12 @@ if uploaded_file is not None:
           with tab1:
             col1, col2 = st.columns(2)
                   
-            @st.cache_data(ttl=3600)
+            @st.cache_resource(ttl=3600)
             def biterm_map(num):
               btmvis_coords = tmp.plot_scatter_topics(topics_coords, size_col='size', label_col='label', topic=num)
               return btmvis_coords
                   
-            @st.cache_data(ttl=3600)
+            @st.cache_resource(ttl=3600)
             def biterm_bar(num):
               terms_probs = tmp.calc_terms_probs_ratio(phi, topic=num, lambda_=1)
               btmvis_probs = tmp.plot_terms(terms_probs, font_size=12)
@@ -230,42 +226,42 @@ if uploaded_file is not None:
     
      #===BERTopic===
     elif method == 'BERTopic':
-        num_btopic = st.slider('Choose number of topics', min_value=4, max_value=20, step=1, on_change=reset_all)
-        @st.cache_data(ttl=3600)
+        num_btopic = st.slider('Choose number of topics', min_value=4, max_value=20, step=1, on_change=reset_resource)
+        @st.cache_resource(ttl=3600)
         def bertopic_vis():
           topic_time = paper.Year.values.tolist()
           cluster_model = KMeans(n_clusters=num_btopic)
-          topic_model = BERTopic(hdbscan_model=cluster_model, language="multilingual", top_n_words=10).fit(topic_abs)
+          topic_model = BERTopic(hdbscan_model=cluster_model, language="multilingual").fit(topic_abs)
           topics, probs = topic_model.fit_transform(topic_abs)
           return topic_model, topic_time, topics, probs
         
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_Topics():
           fig1 = topic_model.visualize_topics()
           return fig1
         
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_Documents():
           fig2 = topic_model.visualize_documents(topic_abs)
           return fig2
 
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_Hierarchy():
           fig3 = topic_model.visualize_hierarchy(top_n_topics=num_btopic)
           return fig3
     
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_Heatmap():
           global topic_model
           fig4 = topic_model.visualize_heatmap(n_clusters=num_btopic-1, width=1000, height=1000)
           return fig4
 
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_Barchart():
           fig5 = topic_model.visualize_barchart(top_n_topics=num_btopic, n_words=10)
           return fig5
     
-        @st.cache_data(ttl=3600)
+        @st.cache_resource(ttl=3600)
         def Vis_ToT():
           topics_over_time = topic_model.topics_over_time(topic_abs, topic_time)
           fig6 = topic_model.visualize_topics_over_time(topics_over_time)
