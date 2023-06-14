645ae84d190e11f708ea783eef834929cf92989e
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-14 09:03:35 +0700

    Update 3 bidi.py

--- pages/3 bidi.py
+++ pages/3 bidi.py
@@ -196,7 +196,6 @@ if uploaded_file is not None:
                 with st.spinner('Visualizing, please wait ....'): 
                      @st.cache_data(ttl=3600)
                      def map_node(uID):
-                        #st.write(uID)
                         res['to'] = res['antecedents'] + ' → ' + res['consequents'] + '\n Support = ' +  res['support'].astype(str) + '\n Confidence = ' +  res['confidence'].astype(str) + '\n Conviction = ' +  res['conviction'].astype(str)
                         res_ant = res[['antecedents','antecedent support']].rename(columns={'antecedents': 'node', 'antecedent support': 'size'}) #[['antecedents','antecedent support']]
                         res_con = res[['consequents','consequent support']].rename(columns={'consequents': 'node', 'consequent support': 'size'}) #[['consequents','consequent support']]
@@ -204,7 +203,6 @@ if uploaded_file is not None:
                         return res_node, res
                      
                      res_node, res = map_node(uID)
-                     st.dataframe(res, use_container_width=True)
 
                      @st.cache_data(ttl=3600)
                      def arul_network(uID):
