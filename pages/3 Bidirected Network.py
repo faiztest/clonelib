8b6e788ea4569f1dcc6c1c226e0374dc74793bef
faiztest <129721483+faiztest@users.noreply.github.com>
2023-12-04 15:04:25 +0700

    Update 3 Bidirected Network.py

--- pages/3 Bidirected Network.py
+++ pages/3 Bidirected Network.py
@@ -172,7 +172,7 @@ if uploaded_file is not None:
             res = res[['antecedents', 'consequents', 'antecedent support', 'consequent support', 'support', 'confidence', 'lift', 'conviction']]
             res['antecedents'] = res['antecedents'].apply(lambda x: ', '.join(list(x))).astype('unicode')
             res['consequents'] = res['consequents'].apply(lambda x: ', '.join(list(x))).astype('unicode')
-            restab['Show'] = True 
+            pd.restab({'Show': [True]}).bool()
             restab = res
             return res, restab
 
